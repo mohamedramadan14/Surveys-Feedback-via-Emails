56352e7b96597e53fc87a307675131d3d7bf4419
Mohamed Ramadan <mohameda7maa@gmail.com>
2022-11-06 11:05:32 +0200

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("/*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html"),
+      path.join(__dirname, "client", "build", "index.html"),
       (err) => {
         if (err) {
           console.log(err);
